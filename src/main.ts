2097755e240e7b5932f77b9db939823d844d3c18
blingbling-110 <523497359@qq.com>
2021-10-18 21:02:52 +0800

    feat: 完成Food类

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
 import './main.scss'
+
+class Food {
+  element: HTMLElement
+
+  constructor () {
+    this.element = document.getElementById('food')!
+  }
+
+  get x () {
+    return this.element.offsetLeft
+  }
+
+  get y () {
+    return this.element.offsetTop
+  }
+
+  change () {
+    this.element.style.left = Math.round(Math.random() * 29) * 10 + 'px'
+    this.element.style.top = Math.round(Math.random() * 29) * 10 + 'px'
+  }
+}
+
+// TODO 测试代码
+const food = new Food()
+console.log(food.x, food.y)
+food.change()
+console.log(food.x, food.y)
+// END 测试代码
